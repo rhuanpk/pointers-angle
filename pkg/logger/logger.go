3c5e80094f471de6fda66beb69239a3ec28d698a
rhuan-pk <rhuan.pksf@gmail.com>
2023-08-03 20:19:21 -0300

    style: log messages repatterned.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -7,9 +7,9 @@ import (
 
 // For all logs, the "Debug" log is the only that has your default output to stderr for in production the user has the possibility to supress it.
 var (
-	Debug = log.New(os.Stderr, "[DEBUG] (logger) ", log.Ldate|log.Ltime|log.Lshortfile)
-	Info  = log.New(os.Stdout, "[INFO] (logger) ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warn  = log.New(os.Stdout, "[WARN] (logger) ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(os.Stdout, "[ERROR] (logger) ", log.Ldate|log.Ltime|log.Lshortfile)
-	Fatal = log.New(os.Stdout, "[FATAL] (logger) ", log.Ldate|log.Ltime|log.Lshortfile)
+	Debug = log.New(os.Stderr, "[DEBUG] ", log.Ltime|log.Lshortfile)
+	Info  = log.New(os.Stdout, "[INFO] ", log.Ltime|log.Lshortfile)
+	Warn  = log.New(os.Stdout, "[WARN] ", log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stdout, "[ERROR] ", log.Ltime|log.Lshortfile)
+	Fatal = log.New(os.Stdout, "[FATAL] ", log.Ltime|log.Lshortfile)
 )
