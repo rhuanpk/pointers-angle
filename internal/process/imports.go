7ae8cb84fa61ba66112085bfa150c4f2bd31399e
rhuan-pk <rhuan.pksf@gmail.com>
2023-08-04 03:01:40 -0300

    feat: add database package.

---
+++ internal/process/imports.go
@@ -0,0 +1,6 @@
+package process
+
+import (
+	// Imports of all project packages which not auto initialization.
+	_ "github.com/rhuanpk/pointers-angle/internal/infra/db"
+)
--- internal/process/init.go
+++
@@ -1,7 +0,0 @@
-package process
-
-import "github.com/rhuanpk/pointers-angle/internal/config"
-
-func init() {
-	config.Load()
-}
