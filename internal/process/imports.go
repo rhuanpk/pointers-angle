32a760caf2c0dcb7770cd90b356a3aa8a9821252
rhuan-pk <rhuan.pksf@gmail.com>
2023-08-07 07:58:27 -0300

    doc: add swagger.

--- internal/process/imports.go
+++ internal/process/imports.go
@@ -2,6 +2,7 @@ package process
 
 import (
 	// Imports of all project packages which not auto initialization.
+	_ "github.com/rhuanpk/pointers-angle/docs"
 	_ "github.com/rhuanpk/pointers-angle/internal/core/clock"
 	_ "github.com/rhuanpk/pointers-angle/internal/infra/db"
 )
